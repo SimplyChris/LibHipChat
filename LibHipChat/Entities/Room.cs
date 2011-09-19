c21415966acfc5e1a2f97e534ca5c2b8f1fc295c
Chris Morgan <chris.morgan357@gmail.com>
2011-09-19 08:29:33 -0500

    Added room and user entities and started the webclient class

--- LibHipChat/Entities/Room.cs
+++ LibHipChat/Entities/Room.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using LibHipChat.Entities;
 
-namespace LibHipChat
+namespace LibHipChat.Entities
 {
     public class Room
     {
---
+++ LibHipChat/WebClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace LibHipChat
+{
+    public class WebClient
+    {
+        private WebRequest _webRequest;
+        //TODO: Use proper configuration 
+        private const string _apiUrl = "https://api.hipchat.com/v1";
+
+        public class WebClient 
+    }
+}
