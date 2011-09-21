bd22ea0d3c4d1aadb0bf7867ec98a5e2d6ab40e8
Chris Morgan <chris.morgan357@gmail.com>
2011-09-20 22:35:53 -0500

    Added ActionKey. Started moving creation of webclient into a factory and added a strategy placeholder

--- LibHipChat/WebClient.cs
+++ LibHipChat/WebClient.cs
@@ -4,10 +4,22 @@ namespace LibHipChat
 {
     public class WebClient
     {
-        private WebRequest _webRequest;
+        private HttpWebRequest _webRequest;
         //TODO: Use proper configuration 
-        private const string _apiUrl = "https://api.hipchat.com/v1";
+        private const string _apiBaseUrl = "https://api.hipchat.com/v1";
 
-        public class WebClient 
+        public WebClient ()
+        {
+
+            
+        }
+        
+        public HttpWebRequest CreateWebRequest ()
+        {
+            //TODO: Append rest url somehow
+            var requestUrl = _apiBaseUrl;
+            var request = (HttpWebRequest)WebRequest.Create(requestUrl);
+            return request;
+        }
     }
 }
