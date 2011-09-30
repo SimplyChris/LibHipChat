69d3d06b806120e6d9107da24b4fa2af5d413af5
Chris Morgan <chris.morgan357@gmail.com>
2011-09-29 22:36:05 -0500

    HipChatConnection now can execute calls for listrooms and listusers and return an xdocument. Integration tests for both calls. Need to add mocking.

--- LibHipChat/HipChatConnection.cs
+++ LibHipChat/HipChatConnection.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Net;
+using System.Xml.Linq;
 using LibHipChat.Helpers;
 
 namespace LibHipChat
@@ -7,13 +9,27 @@ namespace LibHipChat
     public class HipChatConnection : WebRequest
     {
         private HttpWebRequest _webRequest;
-
-
+        private string _responseString = "";
+        
+        
         public String ConnectionUrl { get { return _webRequest.RequestUri.ToString(); } }
+        public String Response { get { return _responseString; } }
 
         public HipChatConnection (String baseApiUrl, HipChatContext context)
         {
-            _webRequest = CreateWebRequest(baseApiUrl, context);            
+            _webRequest = CreateWebRequest(baseApiUrl, context);
+        }
+
+        public XDocument Execute ()
+        {
+            var response =  (HttpWebResponse)_webRequest.GetResponse();                        
+            var responseStream = response.GetResponseStream();
+            var reader = new StreamReader(responseStream);
+            _responseString = reader.ReadToEnd();
+
+            var xDoc = XDocument.Parse(_responseString);
+
+            return xDoc;
         }
         
         private HttpWebRequest CreateWebRequest (String baseApiUrl, HipChatContext context)
---
+++ LibHipChat/HipChatConnectionTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace LibHipChat
+{
+    [TestFixture]
+    public class HipChatConnectionTests
+    {
+        private HipChatConnection _connection;
+        private HipChatConnectionFactory _connectionFactory;
+        private const string apiKey = "fc571e6af2aa7dc9ddd99bc043c3a8";
+        private const string apiUrl = "https://api.hipchat.com/v1/";
+
+        [SetUp]
+        public void Setup ()
+        {
+            _connectionFactory = new HipChatConnectionFactory(apiKey, apiUrl);
+        }
+
+        [Test]
+        public void should_be_able_to_execute_listusers ()
+        {
+            _connection = _connectionFactory.Create(ActionKey.ListUsers);            
+            var response = _connection.Execute();
+        }
+
+        [Test]
+        public void should_be_able_to_execute_listrooms ()
+        {
+            _connection = _connectionFactory.Create(ActionKey.ListRooms);
+            var response = _connection.Execute();
+        }
+    }
+}
